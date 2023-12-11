3e88cc9ba59ea88b8699c79bfe0a81d312605c7d
Taylor O'Brien <toobrien@amazon.com>
2023-12-10 20:12:47 -0800

    .

--- scripts/spread.py
+++ scripts/spread.py
@@ -9,17 +9,55 @@ path.append(".")
 from    util                    import  read_storage
 
 
-# python scripts/spread.py nqh4_sample
+# python scripts/spread.py nqh4_sample 2 0.25
 
 
 if __name__ == "__main__":
 
-    t0  = time()
-    df  = read_storage(argv[1])
+    t0 = time()
 
-    pass
+    fn          = argv[1]
+    precision   = int(argv[2])
+    tick_size   = float(argv[3])
+    rows        = read_storage(fn).rows()
 
-    print(f"elapsed: {time() - t0:0.1f}s")
+    BID = 12
+    ASK = 13
+
+    prev_bid = rows[0][BID]
+    prev_ask = rows[0][ASK]
+
+    spreads = []
+
+    for row in rows[1:]:
+
+        bid = row[BID]
+        ask = row[ASK]
+
+        if bid != prev_bid or ask != prev_ask:
+
+            spread = prev_ask - prev_bid
+
+            prev_bid = bid
+            prev_ask = ask
+
+            spreads.append(spread)
+
+    print(f"avg:     {np.average(spreads):0.{precision}f}")
+    print(f"median:  {np.median(spreads):0.{precision}f}")
+    print(f"min:     {min(spreads):0.{precision}f}")
+    print(f"max:     {max(spreads):0.{precision}f}")
+    print(f"samples: {len(spreads)}")
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Histogram(x = spreads, nbinsx = int((max(spreads) - min(spreads)) / tick_size))
+    )
+
+    fig.show()
+
+    print(f"\nelapsed: {time() - t0:0.1f}s")
 
     
     
