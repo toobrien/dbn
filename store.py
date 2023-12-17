58c4676be41ef60ddb7781e6fdccfe3ff7f6fb5b
Taylor O'Brien <toobrien@amazon.com>
2023-12-17 14:41:56 -0800

    .

--- store.py
+++ store.py
@@ -3,23 +3,24 @@ from    sys         import  argv
 from    time        import  time
 
 
-# python store.py mbp-1 2023-12-07 2023-12-08 raw_symbol nqh4_sample create NQH4
+# python store.py create mbp-1 2023-12-07 2023-12-08 raw_symbol nqh4_sample NQH4
+# python store.py new mbo 2023-12-15 zrh4_mbo
 
 
 if __name__ == "__main__":
 
     t0      = time()
     client  = Historical()
-    schema  = argv[1]
+    mode    = argv[1]
     rng     = client.metadata.get_dataset_range(dataset = "GLBX.MDP3")
-    start   = argv[2] if argv[2] != "-" else rng["start_date"]
-    end     = argv[3] if argv[3] != "-" else rng["end_date"]
-    stype   = argv[4]
-    fn      = f"./storage/{argv[5]}.dbn.zst"
-    mode    = argv[6]
 
     if mode == "create":
 
+        schema  = argv[2]
+        start   = argv[3] if argv[3] != "-" else rng["start_date"]
+        end     = argv[4] if argv[4] != "-" else rng["end_date"]
+        stype   = argv[5]
+        fn      = f"./storage/{argv[6]}.dbn.zst"
         symbols = argv[7:]
 
         data = client.timeseries.get_range(
@@ -35,14 +36,24 @@ if __name__ == "__main__":
     
     elif mode == "new":
 
-        data        = DBNStore.from_file(path = fn)
-        dataset     = data.dataset
-        schema      = data.schema
-        start       = data.end
-        stype_in    = data.stype_in
-        symbols     = data.symbology["symbols"]
-        end         = rng["end_date"]
+        fn  = f"./storage/{argv[3]}.dbn.zst"
+        old = DBNStore.from_file(path = fn)
+        end = argv[2] if argv[2] != "-" else rng["end_date"]
 
         pass
-    
+
+        new = client.timeseries.get_range(
+            dataset     = "GLBX.MDP3",
+            schema      = old.schema,
+            stype_in    = old.stype_in,
+            symbols     = old.symbols,
+            start       = old.end,
+            end         = end
+
+        )
+
+        # TODO: append old + new into "data"
+
+        # data.to_file(path = fn)
+
     print(f"elapsed:  {time() - t0: 0.1f}s")
