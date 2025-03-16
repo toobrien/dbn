98150cbe1828b0088d1b52aa3a63fbbb874a9e83
Taylor O'Brien <toobrien@amazon.com>
2025-03-16 16:24:03 -0700

    .

--- scripts/agg_plot.py
+++ scripts/agg_plot.py
@@ -1,5 +1,6 @@
 import  os
 import  plotly.graph_objects    as      go
+from    plotly.subplots         import  make_subplots
 import  polars                  as      pl
 from    sys                     import  argv, path
 from    time                    import  time
@@ -19,25 +20,31 @@ if __name__ == "__main__":
     fn              = os.path.join(".", "csvs", f"{sym}_trades.csv")
     df              = pl.read_csv(fn).select(
                         [
-                            "ts_event", 
-                            "action", 
-                            "side", 
-                            "price", 
+                            "ts_event",
+                            "action",
+                            "side",
+                            "price",
                             "size"
                         ]
                     )
+    df              = df.filter(pl.col("side") != "N")
     df              = strptime(df, "ts_event", "ts", "%Y-%m-%dT%H:%M:%S.%f", "America/Los_Angeles")
     df              = df.with_row_index(name = "index")
     x, y, z, t, s   = combine_trades(df.select([ "index", "ts", "price", "size", "side" ]))
     c_map           = { "A": "#0000FF", "B": "#FF0000", "N": "#CCCCCC" }
     c               = [ c_map[i] for i in s ]
 
-    fig = go.Figure()
+    fig = make_subplots(
+            rows                = 2, 
+            cols                = 1,
+            row_heights         = [ 0.75, 0.25 ],
+            vertical_spacing    = 0.025
+        )
     
     fig.add_trace(
         go.Scattergl(
             {
-                "name":         sym,
+                "name":         f"{sym} tick",
                 "x":            x,
                 "y":            y,
                 "mode":         "markers",
@@ -50,8 +57,46 @@ if __name__ == "__main__":
                                     "sizemin":  4
                                 }
             }
-        )
+        ),
+        row = 1,
+        col = 1
     )
+
+    date    = [ ts.split("T")[0] for ts in t ]
+    df_     = pl.DataFrame(
+                { 
+                    "date":     date,
+                    "price":    y
+                }
+            ).group_by(
+                "date"
+            ).agg(
+                [
+                    pl.col("price").first().alias("open"),
+                    pl.col("price").max().alias("high"),
+                    pl.col("price").min().alias("low"),
+                    pl.col("price").last().alias("close")
+                ]
+            )
+
+    fig.add_trace(
+        go.Candlestick(
+            {
+                "name":         f"{sym} daily",
+                "x":            df_["date"],
+                "open":         df_["open"],
+                "high":         df_["high"],
+                "low":          df_["low"],
+                "close":        df_["close"],
+                "increasing":   { "line": { "color": "blue" } },
+                #"decreasing":   { "line": { "color": "blue" } }
+            }
+        ),
+        row = 2,
+        col = 1
+    )
+
+    fig.update_layout(xaxis2_rangeslider_visible = False)
 
     fig.show()
 
